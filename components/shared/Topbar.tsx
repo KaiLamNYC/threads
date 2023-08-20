e80fba5fbfc2a88bae6280d388161796f0467e1d
Kai Lam <kailamnyc@gmail.com>
2023-08-20 09:39:13 -0400

    working on shadcn form and onbaording

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -29,6 +30,7 @@ function Topbar() {
 				<OrganizationSwitcher
 					// PASSING SOME TAILWIND PROPS
 					appearance={{
+						baseTheme: dark,
 						elements: {
 							organizationSwitcherTrigger: "py-2 px-4",
 						},
